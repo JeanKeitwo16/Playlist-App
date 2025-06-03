2ba9b1e260956574e32b47a36a5b8cb3fe5abead
Luis <luis@example.com>
2025-06-02 22:05:08 -0300

    App rodando

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "br.edu.ifsul.flutter_playlist"
+    namespace = "com.farteche.player_musica"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "br.edu.ifsul.flutter_playlist"
+        applicationId = "com.farteche.player_musica"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/br/edu/ifsul/flutter_playlist/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package br.edu.ifsul.flutter_playlist
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
